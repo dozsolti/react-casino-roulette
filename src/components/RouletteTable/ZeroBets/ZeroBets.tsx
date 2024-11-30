3022c20be3fe7e75d533e0bff521de7e2ff62d43
Domotor Zsolti <d.zsolti@yahoo.com>
2024-11-30 14:25:21 +0200

    refactor: updated the chips components on zeros to make a double zero optional.

--- src/components/RouletteTable/ZeroBets/ZeroBets.tsx
+++ src/components/RouletteTable/ZeroBets/ZeroBets.tsx
@@ -1,145 +1,144 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import type { FC } from 'react';
 
 import { Chip } from '../Chip';
 
 import { ACTION_TYPES } from '../../../constants';
 import { findChipIcon, shouldRenderChip } from '../../../helpers';
-import { RouletteTableContext } from '../../../context';
+import { IRouletteTableContextProps, RouletteTableContext } from '../../../context';
+import { TableLayoutType } from '../types';
+import { ChipRenderer } from '../utils/ChipRenderer';
 
-export const ZeroBets: FC = () => {
-  const { onBetCatcherHover, bets } = useContext(RouletteTableContext);
+interface IZeroBetsProps {
+  layoutType: TableLayoutType;
+}
+
+export const ZeroBets: FC<IZeroBetsProps> = ({ layoutType }) => {
+  const { bets } = useContext(RouletteTableContext);
 
   return (
     <>
-      {['0', '00'].map((number) => (
-        <div
-          key={`zero-item-${number}`}
-          className="zero-item"
-          data-action={ACTION_TYPES[number as '0' | '00']}
-          data-bet={number}
-        >
-          {/* start chip */}
-          {number === '0' && (
-            <>
-              <div
-                className="spleet-bet-catcher"
-                onMouseEnter={onBetCatcherHover}
-                onMouseLeave={onBetCatcherHover}
-                data-action={ACTION_TYPES.STREET}
-                data-highlight="0-00-2"
-                style={{ left: 'auto', right: -15, zIndex: 13 }}
-              />
-              {shouldRenderChip('0-00-2', bets) === true && (
-                <Chip
-                  position="right-top"
-                  icon={findChipIcon('0-00-2', bets)}
-                />
-              )}
-            </>
-          )}
-          {/* end chip */}
-          {/* start chip */}
-          <div
-            className={`corner-bet-catcher ${number === '0' ? 'bottom' : ''}`}
-            onMouseEnter={onBetCatcherHover}
-            onMouseLeave={onBetCatcherHover}
-            data-action={ACTION_TYPES.BASKET_US}
-            data-highlight={number === '0' ? '0-00-1-2-3' : '00-0-1-2-3'}
-            style={{ zIndex: 14 }}
-          />
-          {shouldRenderChip(
-            number === '0' ? '0-00-1-2-3' : '00-0-1-2-3',
-            bets,
-          ) === true && (
-            <Chip
-              position={number === '0' ? 'right-bottom' : 'right-top'}
-              icon={findChipIcon(
-                number === '0' ? '0-00-1-2-3' : '00-0-1-2-3',
-                bets,
-              )}
-            />
-          )}
-          {/* end chip */}
-          {/* start chip */}
-          {number === '0' && (
-            <>
-              <div
-                className="split-up-bet-catcher-top"
-                onMouseEnter={onBetCatcherHover}
-                onMouseLeave={onBetCatcherHover}
-                data-action={ACTION_TYPES.ROW}
-                data-highlight="0-00"
-              />
-              {shouldRenderChip('0-00', bets) === true && (
-                <Chip position="center-top" icon={findChipIcon('0-00', bets)} />
-              )}
-            </>
-          )}
-          {/* end chip */}
-          {/* start chip */}
-          <div
-            className="split-up-bet-catcher-right"
-            onMouseEnter={onBetCatcherHover}
-            onMouseLeave={onBetCatcherHover}
-            data-action={ACTION_TYPES.SPLIT}
-            data-highlight={`${number}-${number === '0' ? 2 : 3}`}
-            style={{
-              zIndex: number === '00' ? 12 : '',
-              height: number === '00' ? '85px' : '',
-            }}
-          />
-          {shouldRenderChip(`${number}-${number === '0' ? 2 : 3}`, bets) ===
-            true && (
-            <Chip
-              position={
-                number === '0'
-                  ? 'right-top-with-no-offset'
-                  : 'right-top-with-offset'
-              }
-              icon={findChipIcon(`${number}-${number === '0' ? 2 : 3}`, bets)}
-            />
-          )}
-          {/* end chip */}
-          {/* start chip */}
-          <div
-            className="split-up-bet-catcher-right"
-            onMouseEnter={onBetCatcherHover}
-            onMouseLeave={onBetCatcherHover}
-            data-action={ACTION_TYPES.SPLIT}
-            data-highlight={`${number}-${number === '0' ? 1 : 2}`}
-            style={{ height: 85, top: 'auto', bottom: 0 }}
-          />
-          {shouldRenderChip(`${number}-${number === '0' ? 1 : 2}`, bets) ===
-            true && (
-            <Chip
-              position={
-                number === '0'
-                  ? 'right-bottom-with-offset'
-                  : 'right-bottom-with-no-offset'
-              }
-              icon={findChipIcon(`${number}-${number === '0' ? 1 : 2}`, bets)}
-            />
-          )}
-          {/* end chip */}
-          {/* start chip */}
-          <div
-            className={
-              number === '0' ? 'basket-catcher-bottom' : 'basket-catcher-top'
-            }
-            onMouseEnter={onBetCatcherHover}
-            onMouseLeave={onBetCatcherHover}
-            data-action={ACTION_TYPES.BASKET_US}
-            data-highlight={`${number === '0' ? '0-00' : '00-0'}-1-2-3`}
-            style={{ left: -3 }}
-          />
-          {/* end chip */}
-          <div className="value">{number}</div>
-          {shouldRenderChip(number, bets) && (
-            <Chip position="center" icon={findChipIcon(number, bets)} />
-          )}
-        </div>
-      ))}
+      <RenderSingleZero layoutType={layoutType} bets={bets} />
+      {layoutType == 'american' && <RenderDoubleZero layoutType={layoutType} bets={bets} />}
     </>
   );
 };
+
+
+function RenderDoubleZero({ layoutType, bets }: { layoutType: TableLayoutType, bets: IRouletteTableContextProps['bets'] }) {
+  return (<div
+    key={`zero-item-00`}
+    className={`zero-item ${layoutType === 'european' ? 'single-zero' : ''}`}
+    data-action={ACTION_TYPES['00']}
+    data-bet={'00'}
+  >
+
+    {/* Chips - 00-3 */}
+    {<ChipRenderer
+      cName="split-up-bet-catcher-right"
+      action={ACTION_TYPES.SPLIT}
+      highlight={`00-3`}
+      style={{
+        zIndex: 12,
+        height: '85px',
+      }}
+      chipPosition={'right-top-with-offset'}
+    />}
+
+    {/* end chip */}
+
+    {/* Chips - 00-2*/}
+    {<ChipRenderer
+      cName='split-up-bet-catcher-right'
+      action={ACTION_TYPES.SPLIT}
+      highlight={`00-2`}
+      style={{ height: 85, top: 'auto', bottom: 0 }}
+      chipPosition={'right-bottom-with-no-offset'}
+    />}
+    {/* end chip */}
+
+
+    <div className="value">00</div>
+    {shouldRenderChip('00', bets) && (
+      <Chip position="center" icon={findChipIcon('00', bets)} />
+    )}
+  </div>)
+}
+
+function RenderSingleZero({ layoutType, bets }: { layoutType: TableLayoutType, bets: IRouletteTableContextProps['bets'] }) {
+  return (<div
+    key={`zero-item-0`}
+    className={`zero-item ${layoutType === 'european' ? 'single-zero' : ''}`}
+    data-action={ACTION_TYPES['0']}
+    data-bet={'0'}
+  >
+    {/* Chips - 0-00-2 */}
+    {layoutType === 'american' &&
+      <ChipRenderer
+        cName='spleet-bet-catcher'
+        action={ACTION_TYPES.STREET}
+        highlight='0-00-2'
+        style={{ left: 'auto', right: -15, zIndex: 13 }}
+        chipPosition="right-top"
+      />}
+    {/* end chip */}
+
+    {/* Chips - Line between zeros - 00-0 */}
+    {layoutType === 'american' &&
+      <ChipRenderer
+        cName="split-up-bet-catcher-top"
+        action={ACTION_TYPES.ROW}
+        highlight="00-0"
+        chipPosition="center-top"
+      />
+    }
+    {/* end chip */}
+
+    {/* Chips - 0-2*/}
+    {<ChipRenderer
+      cName="split-up-bet-catcher-right"
+      action={ACTION_TYPES.SPLIT}
+      highlight={`0-2`}
+      chipPosition={'right-top-with-no-offset'}
+    />}
+    {/* end chip */}
+
+
+
+
+    {/* Chips - 0-1*/}
+    {<ChipRenderer
+      cName='split-up-bet-catcher-right'
+      action={ACTION_TYPES.SPLIT}
+      highlight={`0-1`}
+      style={{ height: 85, top: 'auto', bottom: 0 }}
+      chipPosition={'right-bottom-with-offset'}
+    />}
+    {/* end chip */}
+
+    {/* Chips - Line below 0 - 00-0-1-2-3 - street */}
+    {
+      <ChipRenderer
+        cName={`basket-catcher-bottom`}
+        action={ACTION_TYPES.BASKET_US}
+        highlight={layoutType === 'american' ? '00-0-1-2-3' : '0-1-2-3'}
+        style={{ left: -3 }}
+        hideChips={true}
+      />}
+    {/* end chip */}
+    {/* Chips - bottom right corner below 0 - street */}
+    {<ChipRenderer
+      cName={`corner-bet-catcher bottom`}
+      action={ACTION_TYPES.BASKET_US}
+      highlight={layoutType === 'american' ? '00-0-1-2-3' : '0-1-2-3'}
+      style={{ zIndex: 14 }}
+      chipPosition={'right-bottom'}
+    />}
+    {/* end chip */}
+
+    <div className="value">0</div>
+    {shouldRenderChip('0', bets) && (
+      <Chip position="center" icon={findChipIcon('0', bets)} />
+    )}
+  </div>)
+}
---
+++ src/components/RouletteTable/utils/ChipRenderer.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { shouldRenderChip, findChipIcon } from "../../../helpers";
+import { Chip } from "../Chip";
+import { RouletteTableContext } from "../../../context";
+
+interface ChipRendererProps {
+    cName: string,
+    action: string,
+    highlight: string,
+    style?: any,
+    chipPosition?: any,
+    hideChips?: boolean,
+}
+
+export function ChipRenderer({ cName, action, highlight, style = {}, chipPosition = '', hideChips = false }: ChipRendererProps) {
+    const { onBetCatcherHover, bets } = useContext(RouletteTableContext);
+
+    return (
+        <>
+            <div
+                className={cName}
+                onMouseEnter={onBetCatcherHover}
+                onMouseLeave={onBetCatcherHover}
+                data-action={action}
+                data-highlight={highlight}
+                style={style}
+            />
+            {!hideChips && shouldRenderChip(highlight, bets) === true && (
+                <Chip
+                    position={chipPosition}
+                    icon={findChipIcon(highlight, bets)}
+                />
+            )}
+        </>
+    );
+}
